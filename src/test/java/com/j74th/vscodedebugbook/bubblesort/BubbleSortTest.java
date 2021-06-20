f29f97078b0d176016b9e3d2094cbeb9c072c29f
Atsushi Morimoto (74th) <site@74th.tech>
2021-06-20 04:13:44 +0000

    update

--- src/test/java/com/j74th/vscodedebugbook/bubblesort/BubbleSortTest.java
+++ src/test/java/com/j74th/vscodedebugbook/bubblesort/BubbleSortTest.java
@@ -6,18 +6,17 @@ import junit.framework.TestSuite;
 
 public class BubbleSortTest extends TestCase {
 
-	public BubbleSortTest(String testName){
-        super( testName );
+	public BubbleSortTest(String testName) {
+		super(testName);
 	}
 
-    public static Test suite()
-    {
-        return new TestSuite( BubbleSortTest.class );
-    }
+	public static Test suite() {
+		return new TestSuite(BubbleSortTest.class);
+	}
 
-    public void testBubbleSort() {
+	public void testBubbleSort() {
 		int[] list = new int[4];
-		list[0] = 4;
+		list[0] = 2;
 		list[1] = 3;
 		list[2] = 2;
 		list[3] = 1;
@@ -25,25 +24,25 @@ public class BubbleSortTest extends TestCase {
 		BubbleSort sorter = new BubbleSort();
 		sorter.Sort(list);
 
-		assertEquals(1,list[0]);
-		assertEquals(2,list[1]);
-		assertEquals(3,list[2]);
-		assertEquals(4,list[3]);
-    }
+		assertEquals(1, list[0]);
+		assertEquals(2, list[1]);
+		assertEquals(3, list[2]);
+		assertEquals(4, list[3]);
+	}
 
-    public void testThrowException() {
+	public void testThrowException() {
 		int[] list = new int[4];
-		list[0] = 4;
+		list[0] = 3;
 		list[1] = 3;
 		list[2] = 2;
 		list[3] = 1;
 
-		try{
+		try {
 			ExceptionThrow sorter = new ExceptionThrow();
 			sorter.Sort(list);
 			fail();
-		}catch(Exception e){
+		} catch (Exception e) {
 		}
 
-    }
+	}
 }
